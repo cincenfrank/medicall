5fc61be6cc40a0362e242e85369e458eb53d6112
nicosanji <n.sangiorgio92@gmail.com>
2022-02-07 17:21:39 +0100

    update scaffolding

--- public/js/app.js
+++ public/js/app.js
@@ -40255,8 +40255,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/cincenfrank/Developer/boolean/medicall/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/cincenfrank/Developer/boolean/medicall/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\Boolean\medicall\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\Boolean\medicall\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
