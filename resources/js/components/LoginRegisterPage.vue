d3a7fe65bc3e5d1e66a41954151e91870dcea169
cincenfrank <fpetrangeli1@gmail.com>
2022-02-11 21:58:47 +0100

    added responsive vue page component for auth section

---
+++ resources/js/components/LoginRegisterPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="login-register-page">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-12 col-sm-9 col-md-7 col-lg-5 col-xl-4">
+          <LoginRegisterContainer>
+            <slot></slot>
+          </LoginRegisterContainer>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LoginRegisterContainer from "./LoginRegisterContainer.vue";
+export default {
+  components: { LoginRegisterContainer },
+  name: "LoginRegisterPage",
+};
+</script>
+
+<style lang="scss" scoped>
+.login-register-page {
+  position: relative;
+  background-color: rgba(0, 0, 0, 0.5);
+  &::after {
+    position: absolute;
+    z-index: -1;
+    content: "";
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url("/img/bg-login.jpg");
+    background-position: center;
+    background-size: cover;
+  }
+}
+</style>
+
