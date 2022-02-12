cb233d5e841746a66f939c36d79e2923abe9abfa
nicosanji <n.sangiorgio92@gmail.com>
2022-02-12 17:16:07 +0100

    start branch service-page

---
+++ resources/views/pages/guest/service.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Service</title>
+</head>
+
+<body>
+    <ul>
+        <li>Navbar</li>
+        <li>Jumbo = (services id img_path) + (service id name )</li>
+        <li>DescriptionBlock = (services id description)</li>
+        <li>CardDoctor = (doctor id + services id + subscription id)</li>
+        <li>TheFooter</li>
+    </ul>
+</body>
+
+</html>
