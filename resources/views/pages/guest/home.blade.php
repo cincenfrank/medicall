197ee011f9c3515b41b0c6669141d77126117106
Frameck <fra9879@gmail.com>
2022-02-14 19:18:36 +0100

    created homepage and cardDoctor component

--- resources/views/pages/guest/home.blade.php
+++ resources/views/pages/guest/home.blade.php
@@ -1,1 +1,27 @@
-<h1>Ciao da MediCall</h1>
+@extends('layouts.app')
+{{-- <Navbar></Navbar> --}}
+
+@section('content')
+<jumbo-home></jumbo-home>
+
+<homepage-section
+:title="'Our best doctors'"
+:subtitle="'best in class'"
+:description="'Descrizione della section dei dottori'"
+>
+    <div class="bottom">
+        {{-- carousel with card-doctor --}}
+        <card-doctor></card-doctor>
+    </div>
+</homepage-section>
+
+<homepage-section
+:title="'Our services'"
+:subtitle="'best in class'"
+:description="'Descrizione della section dei servizi'"
+>
+    <div class="bottom">
+        <h1>Carosello con lista dei servizi</h1>
+    </div>
+</homepage-section>
+@endsection
