9dfdf3debcd2fe0470d76434637f6e9f0865484f
cincenfrank <fpetrangeli1@gmail.com>
2022-02-14 18:25:34 +0100

    created carousel vue component

--- resources/js/components/Carousel.vue
+++ resources/js/components/Carousel.vue
@@ -1,13 +1,157 @@
 <template>
-  
+  <!-- TODO decide if it might be better to use bootstrap col logic instead of visibleItemsNumber props for responsiveness  -->
+  <!-- Carousel container with custom style in order to inject css variables -->
+  <div
+    class="carousel d-flex align-items-center position-relative"
+    :style="styleProps"
+  >
+    <!-- Flex row with overflow hidden in order to avoid the presence of the scrollbar
+    and with a ref tag that is used to handle the scroll with js.
+    If the number of items to display is lower than the number of items per row, the items will be centered -->
+    <div
+      class="d-flex overflow-hidden carousel-container mx-3"
+      :class="
+        children.length < visibleItemsNumber ? 'justify-content-center' : ''
+      "
+      ref="scrolled"
+    >
+      <!-- @slot tag to accept carousel items -->
+      <slot></slot>
+    </div>
+    <div
+      class="btn btn-custom-blue position-absolute rounded rounded-circle"
+      @click="onScrollRequested(false)"
+    >
+      &lt;
+    </div>
+    <div
+      class="btn btn-custom-blue position-absolute end-0 rounded rounded-circle"
+      @click="onScrollRequested(true)"
+    >
+      &gt;
+    </div>
+  </div>
 </template>
 
 <script>
+import CardService from "./partials/CardService.vue";
 export default {
+  components: { CardService },
+  props: {
+    /**
+     * Integer that the number of object that will be visible inside a single carousel screen
+     */
+    visibleItemsNumber: Number,
+  },
+  data() {
+    return {
+      /**
+       * Variable that tracks the scroll position offset. It starts to 0 by default
+       */
+      scrollPosition: 0,
+      /**
+       * Array that contains the HTML elements passed to this components inside the slot tag.
+       * It is populated on mounted.
+       */
+      children: [],
+    };
+  },
+  methods: {
+    /**
+     * Triggers when the user clicks on a navigation button of the carousel.
+     * Checks the scroll position and execute the requested scroll.
+     *
+     * @property {Boolean} isForward boolean that indicates if the requested scroll id forward (true) or backward (false).
+     */
+    onScrollRequested(isForward) {
+      // get the html element of the container through ref attribute
+      let element = this.$refs.scrolled;
+      let singleItemWidth = element.scrollWidth / this.childrenCount;
 
-}
+      if (isForward) {
+        // case when the user requested a forward scroll
+
+        // calculates a new scroll position by adding to the actual position the result of the
+        // multiplication between the space occupied by a single item and the number of items
+        // that are visible in a single carousel screen
+        const newScrollPosition =
+          this.scrollPosition + singleItemWidth * this.visibleItemsNumber;
+
+        // check the scroll position in order to avoid to obtain values bigger than the last useful
+        // scroll width value, that is equal to the max scroll with - the size of a default carousel screen
+        if (this.scrollPosition > element.scrollWidth - singleItemWidth * 4) {
+          this.scrollPosition = element.scrollWidth - singleItemWidth * 4;
+        } else {
+          this.scrollPosition = newScrollPosition;
+        }
+      } else {
+        // case when the user requested a forward scroll
+
+        // calculates a new scroll position by subtracting to the actual position the result of the
+        // multiplication between the space occupied by a single item and the number of items
+        // that are visible in a single carousel screen
+        const newScrollPosition =
+          this.scrollPosition - singleItemWidth * this.visibleItemsNumber;
+
+        // check the scroll position in order to avoid to obtain values smaller than 0.
+        if (newScrollPosition < 0) {
+          this.scrollPosition = 0;
+        } else {
+          this.scrollPosition = newScrollPosition;
+        }
+      }
+
+      // executes the scroll
+      element.scrollTo({
+        top: 0,
+        left: this.scrollPosition,
+        behavior: "smooth",
+      });
+    },
+    /**
+     * Initialize the children variables by creating a list of HTML objects that are children of the element
+     * with the value of 'scrolled' in the ref attribute. This function is called on mounted
+     */
+    getChildren() {
+      this.children = this.$refs.scrolled.children;
+    },
+  },
+  computed: {
+    /**
+     * number of items that are passed in the slot tag
+     */
+    childrenCount: function () {
+      return this.children.length;
+    },
+    /**
+     * Object used to create a css variable to set the carousel's items width.
+     * It is injected in the HTML code with a script tag
+     */
+    styleProps() {
+      return {
+        "--carousel-item-width": 100 / this.visibleItemsNumber + "%",
+      };
+    },
+  },
+  mounted() {
+    /**
+     * Initialize children variable
+     */
+    this.getChildren();
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
+.carousel {
+  .carousel-container {
+    & > * {
+      width: var(--carousel-item-width);
+    }
+    .carousel-element {
+      flex-wrap: nowrap;
+      flex-shrink: 0;
+    }
+  }
+}
 </style>
