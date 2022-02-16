7e8acaeb63ed04068dd1c29ae6cb074865d11c84
Frameck <fra9879@gmail.com>
2022-02-16 22:12:51 +0100

    created queries for get data from database

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Review;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view('pages.dashboard.dashboard');
+        // gets the messages for one doctor and groups them by day
+        $messagesCountGroupedByDate = DB::select("SELECT DATE_FORMAT(created_at, '%d/%m/%Y') as `date`, COUNT(id) as `count` FROM `messages` WHERE user_id = 1 GROUP BY DATE_FORMAT(created_at, '%d/%m/%Y');");
+
+        // gets the reviews for one doctor and groups them by day
+        $reviewsCountGroupedByDate = DB::select("SELECT DATE_FORMAT(created_at, '%d/%m/%Y') as `date`, COUNT(id) as `count` FROM `reviews` WHERE user_id = 1 GROUP BY DATE_FORMAT(created_at, '%d/%m/%Y');");
+
+        // calculate the vote avg for the reviews for the doctor
+        $voteAvg = Review::avg('rating');
+
+        // gets the active subscription for the doctor
+        $activeSubscription = User::where('id', '=', '39')->with('subscriptions')->whereHas('subscriptions', function($param) {
+            $param->where('expiration_date', '>', Date::now());
+        })->get()->toArray();
+
+        $chartsData = [
+            'messages_time' => $messagesCountGroupedByDate,
+            'reviews_time' => $reviewsCountGroupedByDate,
+            'vote_avg' => $voteAvg,
+            'active_subscription' => $activeSubscription
+        ];
+        return view('pages.dashboard.dashboard', compact('chartsData'));
     }
 }
