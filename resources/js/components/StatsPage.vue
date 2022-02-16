1ae875072430f62daf7c8a9364392657de578fb3
Frameck <fra9879@gmail.com>
2022-02-16 22:15:14 +0100

    imported statsPage (incomplete) in blade

--- resources/js/components/StatsPage.vue
+++ resources/js/components/StatsPage.vue
@@ -1,13 +1,54 @@
 <template>
-  
+    <div>
+        <div class="card w-50">
+            <div class="card-header">Messaggi nel tempo</div>
+            <div class="card-body">
+                <bar-chart :chartData="chartsData[0]"></bar-chart>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
+    name: 'StatsPage',
+    props: { rawChartsData: Array },
+    data() {
+        return {
+            chartsData: []
+        }
+    },
+    methods: {
+        prepareDataForChartJs(datasets_label) {
+            const labels = [] //date
+            const datasets = [ //valori
+                {
+                    label: datasets_label,
+                    backgroundColor: '#f87979',
+                    data: []
+                }
+            ]
 
-}
+            this.rawChartsData.forEach(el => {
+                labels.push(el.date)
+                datasets
+            })
+        }
+        // chartData: {
+        //     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        //     datasets: [
+        //         {
+        //             label: 'GitHub Commits',
+        //             backgroundColor: '#f87979',
+        //             data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+        //         }
+        //     ]
+        // },
+    },
+    mounted() {
+        this.prepareDataForChartJs()
+    }
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- resources/views/pages/dashboard/dashboard.blade.php
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -1,12 +1,6 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<div class="card">
-    <div class="card-header">
-        Chart.js
-    </div>
-    <div class="card-body">
-        <line-chart></line-chart>
-    </div>
-</div>
+{{dd($chartsData)}}
+<stats-page :chartsData="{{ json_encode($chartsData) }}"></stats-page>
 @endsection
