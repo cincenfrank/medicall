5b7ed89d799de0d8a56e3542504bb727a9323efb
Frameck <fra9879@gmail.com>
2022-02-16 22:13:20 +0100

    modified name of component

--- resources/js/components/partials/BarChart.vue
+++ resources/js/components/partials/BarChart.vue
@@ -6,26 +6,31 @@ export default {
     name: 'LineChart',
     components: { VueCharts, Bar, Line, mixins },
     extends: Bar,
+    props: {
+        chartData: Object
+    },
     // mixins: [mixins.reactiveProp],
     data() {
         return {
-            chartData: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-                datasets: [
-                    {
-                        label: 'GitHub Commits',
-                        backgroundColor: '#f87979',
-                        data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
-                    }
-                ]
-            },
+            // chartData: {
+            //     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            //     datasets: [
+            //         {
+            //             label: 'GitHub Commits',
+            //             backgroundColor: '#f87979',
+            //             data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+            //         }
+            //     ]
+            // },
             options: {
                 plugins: {
                     title: {
                         display: true,
                         text: 'Custom Chart Title'
                     }
-                }
+                },
+                maintainAspectRatio: false,
+                responsive: true
             }            
         }
     },
