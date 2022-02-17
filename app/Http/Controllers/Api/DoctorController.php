25f6077f07976d1192f60ccb256db34b33211bd9
nicosanji <n.sangiorgio92@gmail.com>
2022-02-17 11:06:56 +0100

    fix error  "}" in Api/DoctorController

--- app/Http/Controllers/Api/DoctorController.php
+++ app/Http/Controllers/Api/DoctorController.php
@@ -12,14 +12,15 @@ class DoctorController extends Controller
 {
     public function getSponsoredDoctors(Request $request)
     {
-        $sponsoredDoctors = User::with('subscriptions')->whereHas('subscriptions', function($param) {
+        $sponsoredDoctors = User::with('subscriptions')->whereHas('subscriptions', function ($param) {
             $param->where('expiration_date', '>', Date::now());
         })->get();
         return response()->json($sponsoredDoctors);
-
-    public function fetchReviews($id){
-        $user= User::findOrFail($id);
-        $reviewsPerDottore = Review::where('user_id',$user->id)->get();
+    }
+    public function fetchReviews($id)
+    {
+        $user = User::findOrFail($id);
+        $reviewsPerDottore = Review::where('user_id', $user->id)->get();
         return response()->json($reviewsPerDottore);
     }
 }
