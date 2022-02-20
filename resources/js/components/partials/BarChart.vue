a03e295f2653ac7c218c3fade91aab7cf5cfd54a
cincenfrank <fpetrangeli1@gmail.com>
2022-02-20 22:15:06 +0100

    changed name to Line to fix consolle error

--- resources/js/components/partials/BarChart.vue
+++ resources/js/components/partials/BarChart.vue
@@ -1,54 +1,55 @@
 <script>
-import VueCharts from 'vue-chartjs'
-import { Bar, Line, mixins } from 'vue-chartjs'
+import VueCharts from "vue-chartjs";
+import { Bar, Line as ChartLine, mixins } from "vue-chartjs";
 
 export default {
-    name: 'BarChart',
-    components: { VueCharts, Bar, Line, mixins },
-    extends: Bar,
-    props: {
-        chartData: Object
-    },
-    // mixins: [mixins.reactiveProp],
-    data() {
-        return {
-            // oldChartData: {
-            //     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-            //     datasets: [
-            //         {
-            //             label: 'GitHub Commits',
-            //             backgroundColor: '#f87979',
-            //             data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
-            //         }
-            //     ]
-            // },
-            options: {
-                plugins: {
-                    title: {
-                        display: true,
-                        text: 'Custom Chart Title'
-                    }
-                },
-                maintainAspectRatio: false,
-                responsive: true,
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            suggestedMin: 0,    // minimum will be 0, unless there is a lower value.
-                            // OR //
-                            // beginAtZero: true   // minimum value will be 0.
-                        }
-                    }]
-                }
-            }
-        }
-    },
-    mounted() {
-        this.renderChart(this.chartData, this.options)
-    }
-}
+  name: "BarChart",
+  components: { VueCharts, Bar, ChartLine, mixins },
+  extends: Bar,
+  props: {
+    chartData: Object,
+  },
+  // mixins: [mixins.reactiveProp],
+  data() {
+    return {
+      // oldChartData: {
+      //     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      //     datasets: [
+      //         {
+      //             label: 'GitHub Commits',
+      //             backgroundColor: '#f87979',
+      //             data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+      //         }
+      //     ]
+      // },
+      options: {
+        plugins: {
+          title: {
+            display: true,
+            text: "Custom Chart Title",
+          },
+        },
+        maintainAspectRatio: false,
+        responsive: true,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                suggestedMin: 0, // minimum will be 0, unless there is a lower value.
+                // OR //
+                // beginAtZero: true   // minimum value will be 0.
+              },
+            },
+          ],
+        },
+      },
+    };
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
