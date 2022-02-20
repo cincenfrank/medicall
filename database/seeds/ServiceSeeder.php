971364f7657509506ba1fa1ffe79b31c2d69e25f
cincenfrank <fpetrangeli1@gmail.com>
2022-02-20 13:28:02 +0100

    added img_path to service seeder

--- database/seeds/ServiceSeeder.php
+++ database/seeds/ServiceSeeder.php
@@ -14,12 +14,14 @@ class ServiceSeeder extends Seeder
     public function run(Faker $faker)
     {
         $services = ["Allergologia", "Anatomia Patologica", "Andrologia", "Angiologia Medica", "Cardiochirurgia", "Cardiologia", "Cardiologia pediatrica", "Chirurgia Generale", "Chirurgia Maxillo-facciale", "Chirurgia Pediatrica", "Chirurgia Plastica", "Chirurgia Proctologica e Proctologia", "Chirurgia Toracica", "Chirurgia Vascolare", "Dermatologia e Venereologia", "Diabetologia", "Dietologia", "Ecografia e Doppler", "Ematologia", "Endocrinologia", "Fisiatria", "Fisioterapia", "Gastroenterologia", "Genetica Medica", "Geriatria e Gerontologia", "Ginecologia e Ostetricia", "Immunologia", "Infermieristica", "Infettivologia e Malattie Infettive", "Medicina del Dolore", "Medicina dello Sport", "Medicina Estetica", "Medicina Interna", "Medicina Legale", "Medicina Nucleare", "Nefrologia", "Neurochirurgia", "Neurofisiopatologia", "Neurologia", "Neuropsichiatria Infantile", "Oculistica", "Odontoiatria", "Omeopatia e Agopuntura", "Oncologia", "Ortopedia e Traumatologia", "Otorinolaringoiatria", "Pediatria", "Pneumologia e Malattie Respiratorie", "Psichiatria", "Psicologia", "Radiologia Interventistica", "Radiologia TAC e Risonanza", "Reumatologia", "Senologia", "Urologia"];
-
+        $id = 1;
         foreach ($services as $service) {
             $newService = new Service();
             $newService->name = $service;
             $newService->description = $faker->text(200);
+            $newService->img_path = 'img/services/' . $id . '.jpg';
             $newService->save();
+            $id++;
         }
     }
 }
