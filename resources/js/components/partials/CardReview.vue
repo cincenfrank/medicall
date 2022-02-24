15a1f4a6139974a5fbaf7eb6db64099a51c08ac9
alfredochs <chanta.alfredo@gmail.com>
2022-02-24 15:57:45 +0100

    push stars

--- resources/js/components/partials/CardReview.vue
+++ resources/js/components/partials/CardReview.vue
@@ -1,130 +1,129 @@
 <template>
-  <div class="h-100 px-4 py-4">
-    <div class="card-review-container h-100">
-      <div class="card card-review h-100">
-        <div class="card-header text-center position-relative">
-          <div class="doctor-emoji rounded-pill">🩺</div>
-          <a
-            class="card-text card-doctor-link mb-0"
-            :href="`/doctors/${review.user_id}`"
-          >
-            {{ review.user.last_name }}
-            {{ review.user.first_name }}
-          </a>
-        </div>
-        <div class="card-body" @click="flipCard">
-          <div
-            class="
-              card-front
-              d-flex
-              flex-column
-              align-items-center
-              justify-content-between
-            "
-            v-if="isFront"
-          >
-            <stars class="d-inline-block" :ratings="review.rating"></stars>
-            <div class="text-center">
-              <h5 class="card-text mb-0 fw-bold text-center">
-                {{ review.title }}
-              </h5>
-              <a>vedi recensione</a>
-            </div>
+    <div class="h-100 px-4 py-4">
+        <div class="card-review-container h-100">
+            <div class="card card-review h-100">
+                <div class="card-header text-center">
+                    <div class="doctor-emoji rounded-pill">🩺</div>
+                    <a
+                        class="card-text card-doctor-link mb-0"
+                        :href="`/doctors/${review.user_id}`"
+                    >
+                        {{ review.user.last_name }}
+                        {{ review.user.first_name }}
+                    </a>
+                </div>
+                <div class="card-body" @click="flipCard">
+                    <div
+                        class="card-front d-flex flex-column align-items-center justify-content-between"
+                        v-if="isFront"
+                    >
+                        <stars
+                            class="d-inline-block stars mb-3"
+                            :ratings="review.rating"
+                        ></stars>
+                        <div class="text-center">
+                            <h5 class="card-text mb-2 fw-bold text-center">
+                                {{ review.title }}
+                            </h5>
+                            <a class="text-underline">Vedi recensione</a>
+                        </div>
 
-            <!-- <p class="card-text text-truncate">"{{ review.content }}"</p> -->
-            <p class="card-text fst-italic text-end align-self-end mt-3">
-              {{ review.reviewer_name }}
-            </p>
-          </div>
-          <div
-            class="
-              card-back
-              d-flex
-              flex-column
-              align-items-center
-              justify-content-between
-            "
-            v-else
-          >
-            <div>{{ review.content }}</div>
-          </div>
+                        <!-- <p class="card-text text-truncate">"{{ review.content }}"</p> -->
+                        <p
+                            class="card-text fst-italic text-end align-self-end mt-3"
+                        >
+                            {{ review.reviewer_name }}
+                        </p>
+                    </div>
+                    <div
+                        class="card-back d-flex flex-column align-items-center justify-content-between"
+                        v-else
+                    >
+                        <div>{{ review.content }}</div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
 import Stars from "./Stars.vue";
 export default {
-  components: { Stars },
-  data() {
-    return { isFront: true };
-  },
-  props: {
-    review: Object,
-  },
-  methods: {
-    flipCard() {
-      this.isFront = !this.isFront;
+    components: { Stars },
+    data() {
+        return { isFront: true };
     },
-  },
+    props: {
+        review: Object,
+    },
+    methods: {
+        flipCard() {
+            this.isFront = !this.isFront;
+        },
+    },
 };
 </script>
 
 <style lang="scss" scoped>
 .card-review-container {
-  overflow: hidden;
-  border-radius: 5px;
-  width: 100%;
-  aspect-ratio: 268/208;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5);
-  transition: all 0.2s linear;
-  &:hover {
-    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.5);
-    transform: scale(1.01);
-  }
+    overflow: hidden;
+    border-radius: 5px;
+    width: 100%;
+    aspect-ratio: 268/208;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5);
+    transition: all 0.2s linear;
+    &:hover {
+        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.5);
+        transform: scale(1.01);
+    }
 
-  .card-review {
-    // border: 2px solid #12286a;
-    // transition: all 0.5s linear;
-    // &:hover {
-    //   border: 2px solid #bb0f13;
-    // }
-    .card-body {
-      overflow: hidden;
-      a {
-        cursor: pointer;
-        transition: all 0.3s linear;
-        font-weight: bold;
-        &:hover {
-          color: #bb0f13;
+    .card-review {
+        // border: 2px solid #12286a;
+        // transition: all 0.5s linear;
+        // &:hover {
+        //   border: 2px solid #bb0f13;
+        // }
+        .card-body {
+            position: relative;
+            p.card-text {
+                right: 5%;
+                bottom: 5%;
+                position: absolute;
+            }
+            overflow: hidden;
+            a {
+                cursor: pointer;
+                transition: all 0.3s linear;
+                font-weight: bold;
+                &:hover {
+                    color: #bb0f13;
+                }
+            }
+            .card-back {
+                height: 100%;
+                overflow-y: scroll;
+            }
         }
-      }
-      .card-back {
-        height: 100%;
-        overflow-y: scroll;
-      }
-    }
-    .card-header {
-      background-color: #12286a;
-      border-color: #12286a;
-      color: white;
-      font-weight: bold;
-      .doctor-emoji {
-        background-color: white;
-        display: inline-block;
-        width: 23px;
-        position: absolute;
-        left: 10px;
-      }
-      .card-doctor-link {
-        text-decoration: none;
+        .card-header {
+            background-color: #12286a;
+            border-color: #12286a;
+            color: white;
+            font-weight: bold;
+            .doctor-emoji {
+                background-color: white;
+                display: inline-block;
+                width: 23px;
+                position: absolute;
+                left: 10px;
+            }
+            .card-doctor-link {
+                text-decoration: none;
 
-        font-size: 50;
-        color: white;
-      }
+                font-size: 50;
+                color: white;
+            }
+        }
     }
-  }
 }
 </style>
--- resources/js/components/partials/Stars.vue
+++ resources/js/components/partials/Stars.vue
@@ -2,13 +2,13 @@
     <div>
         <slot></slot>
         <ul class="d-flex list-unstyled mb-0">
-            <li class="bg-secondary rounded p-1">
+            <li class="rounded">
                 <i
                     v-for="i in 5"
                     :key="i"
+                    class="text-warning single-star"
                     :class="ratings >= i ? 'fas fa-star' : 'far fa-star'"
                     aria-hidden="true"
-                    style="color: yellow"
                 ></i>
             </li>
         </ul>
@@ -29,4 +29,10 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.single-star {
+    color: #ffd64a;
+    font-size: 18px;
+    padding: 0 2px;
+}
+</style>
